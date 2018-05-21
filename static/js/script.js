79191ac0436de0c6a1d170060b8c37afc7ad2020
Finn Buick <finnbuick@gmail.com>
2018-05-22 09:28:54 +1000

    added css, minor js tweaks

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,23 @@
     /* your code goes here */
 
     $(document).ready(function(){
-        $('#content').append()
+
+    	$.get({
+    	    url: 'http://127.0.0.1:8080/positions',
+    	    headers : {
+             'Access-Control-Allow-Origin' : '*'
+            },
+            success: function(positions){
+                console.log(positions)
+
+                for (var i = 0; i < 10; i++) {
+                    $('#contentpanel').append("<div class='row'>"
+                    + "<a class='jobtitle'>" + positions[i].title + "</a>"
+                    + "<p class'location'>" + positions[i].location + "</p>"
+                    + "<p class'company'>" + positions[i].company + "</p>"
+                    + "</div>")
+                }
+            }
+    	})
     })
 })()
