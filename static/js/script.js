a110f80d00982f4c4797bd4a3e420a8fe6700f0a
Finn Buick <finnbuick@gmail.com>
2018-05-23 22:26:34 +1000

    content now moves left on click

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@
             console.log(positions)
 
             for (var i = 0; i < 10; i++) {
-                $('.content').append("<div class='row' id=" + positions[i].id + ">"
+                $('#content-left').append("<div class='row' id=" + positions[i].id + ">"
                 + "<p class='meta'>" + new Date(positions[i].created_at).toString().substring(0,21) + "</p>"
                 + "<a class='jobtitle'>" + positions[i].title + "</a>"
                 + "<p class'location'><i class='icon' data-feather='map-pin'></i>" + positions[i].location + "</p>"
@@ -22,6 +22,12 @@
                 + "<p class='description'>" + positions[i].description.substring(0,120) + "...</p>"
                 + "</div>")
             }
+
+            $(".row").on('click', function(){
+                $("#content-left").css("margin", "60px 10px 10px 10px")
+                $("#content-left").css("width", "50%")
+                
+            })
 
             feather.replace({ width: 16, height: 16})
         })
