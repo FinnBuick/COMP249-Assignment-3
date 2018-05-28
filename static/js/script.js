ef748d2ead3dac7e15f9103d6d81011c329d7a42
Finn Buick <finnbuick@gmail.com>
2018-05-28 18:57:49 +1000

    added 'close' button

--- static/js/script.js
+++ static/js/script.js
@@ -16,35 +16,55 @@
             for (var i = 0; i < 10; i++) {
                 $('.content-left').append("<div class='row' data-id=" + positions[i].id + ">"
                 + "<p class='meta'>" + new Date(positions[i].created_at).toString().substring(0,21) + "</p>"
-                + "<a class='jobtitle'>" + positions[i].title + "</a>"
+                + "<a class='jobtitle'><b>" + positions[i].title + "</b></a>"
                 + "<p class'location'><i class='icon' data-feather='map-pin'></i>" + positions[i].location + "</p>"
                 + "<p class'company'><i class='icon' data-feather='briefcase'></i>" + positions[i].company + "</p>"
-                + "<p class='description'>" + positions[i].description.substring(0,120) + "...</p>"
+                + "<p class='description'>" + positions[i].description.substring(0,250) + "...</p>"
                 + "</div>")
             }
 
-
+            var id
             var rowClicked = false
             $(".row").on('click', function(){
                 if (!rowClicked) {
 
-                    var id = this.dataset.id
+                    id = this.dataset.id
                     var position = window.app.model.getPosition(id)
 
-                    $(".content").css("float", "left")
-                    $(".content").css("width", "50%")
+                    $(".content").css("width", "49%")
                     $(".content").css("margin-right", "10px")
+                    $(".content-left").css("float", "left")
+                    $(".content-right").css("float", "right")
 
                     $(".content-right").append("<div class='row' id=" + position.id + ">"
+                    + "<span class='x' data-feather='x-circle'></span>"
                     + "<p class='meta'>" + new Date(position.created_at).toString().substring(0,21) + "</p>"
-                    + "<a class='jobtitle'>" + position.title + "</a>"
+                    + "<a class='jobtitle'><b>" + position.title + "</b></a>"
                     + "<p class'location'><i class='icon' data-feather='map-pin'></i>" + position.location + "</p>"
-                    + "<p class'company'><i class='icon' data-feather='briefcase'></i>" + position.company + "</p>"
-                    + "<p class='description'>" + position.description + "...</p>"
+                    + "<p class'company'><i class='icon' data-feat her='briefcase'></i>" + position.company + "</p>"
+                    + "<p class='description'>" + position.description + "</p>"
+                    + "<h2>Apply:</h2>"
+                    + "<form action='/apply' method='POST'>"
+                        + "<input type='text' value=></input>"
                     + "</div>")
 
                     rowClicked = true
+                    feather.replace({ width: 20, height: 20})
+            }
+
+            $(".x").on('click', function(){
+                if (rowClicked) {
+
+                    var position = window.app.model.getPosition(id)
+
+                    $(".content").css("width", "100%")
+                    $(".content").css("margin-right", "0px")
+                    $("#" + id).remove()
+                    rowClicked = false
+                    $(window).scrollTop(0);
                 }
+            })
+
             })
 
             feather.replace({ width: 16, height: 16})
